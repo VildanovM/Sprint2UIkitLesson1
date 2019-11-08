285603aeecbfd1247b02406b49c0a522d963b2a0
Maxim Vildanov <vildanov8@icloud.com>
2019-11-08 19:49:01 +0300

    makeSprint

---
+++ UIKitLesson/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  UIKitLesson
+//
+//  Created by Kaplan Deniz on 11/10/2019.
+//  Copyright © 2019 DK. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    weak var delegate: Coordinator?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = "Второй Controller"
+		view.backgroundColor = .white
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        button.setTitle("Go to third", for: .normal)
+        button.backgroundColor = .gray
+        button.layer.cornerRadius = 20
+        button.center = view.center
+        button.addTarget(self, action: #selector(goToThirdVC), for: .touchUpInside)
+        view.addSubview(button)
+        
+	}
+    
+    @objc func goToThirdVC() {
+        navigationController?.pushViewController(ThirdViewController(), animated: true)
+    }
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		self.navigationController?.setNavigationBarHidden(false, animated: animated)
+	}
+}
---
+++ UIKitLesson/ThirdViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ThirdViewController.swift
+//  UIKitLesson
+//
+//  Created by Максим Вильданов on 08.11.2019.
+//  Copyright © 2019 DK. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    var circleGesture = GradientView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        createSwipeGesture()
+        
+        circleGesture = GradientView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        circleGesture.center = view.center
+        circleGesture.layer.cornerRadius = 100
+        circleGesture.backgroundColor = .white
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(gradientChange(sender:)))
+        circleGesture.addGestureRecognizer(panGesture)
+        view.addSubview(circleGesture)
+    }
+    
+    
+    func createSwipeGesture() {
+        let downSwipe = UISwipeGestureRecognizer(target: self, action: #selector(popViewController))
+        downSwipe.direction = .down
+        view.addGestureRecognizer(downSwipe)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    @objc func gradientChange(sender: UIPanGestureRecognizer) {
+        let locationY = Float(sender.location(in: circleGesture).y)
+        let locationX = Float(sender.location(in: circleGesture).x)
+        let firstColor = UIColor(red: CGFloat(locationY/255), green: CGFloat(locationY/255), blue: CGFloat(locationY/255), alpha: 1)
+        let secondColor = UIColor(red: CGFloat(locationX/255), green: CGFloat(locationX/255), blue: CGFloat(locationX/255), alpha: 1)
+        circleGesture.colors = [firstColor, secondColor]
+        
+    }
+    
+    
+    @objc func popViewController() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+
+}
